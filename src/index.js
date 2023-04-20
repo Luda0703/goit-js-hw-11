fd04140b1ddd1c693b617108cf22921f4c193a36
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-04-20 20:47:57 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,106 +1,64 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
+// import axios from 'axios';
+// import Notiflix from 'notiflix';
+import NewsApiService from "./js/news-api";
+import {renderGallary} from "./js/gallery";
+import LoadMoreBtn from "./js/load-more-btn";
 
 
-KEY = "35573875-4d45445cc9cc07d3b69f02897";
-BASE_URL = "https://pixabay.com/api/";
-URL = `${BASE_URL}?key=${KEY}&q=${inputText}&image_type="photo"&orientation="horizontal"&safesearch="true"`;
-console.log(URL)
-
-
-const form = document.querySelector('#search-form');
+const searchForm = document.querySelector('#search-form');
 const input = document.querySelector('[text]');
 const btn = document.querySelector('[submit]');
 const gallery = document.querySelector('.gallery');
 
-let inputText = ""
+
+const newsApiService = new NewsApiService();
+
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
 
 
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchHits);
 
-function createGalleryItemsMarkup(galleryItems) {
-    
-    return galleryItems
-    .map(({ largeImageURL, tags }) => {
-        return `
-        <div class="gallery">
-        <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
-        </div>`
-    })
-    .join(' ');
+function onSearch (e) {
+    e.preventDefault();
+
+    newsApiService.query = e.currentTarget.searchQuery.value.trim();
+
+    loadMoreBtn.show();
+
+    newsApiService.resetPage();
+    clearGalleryContainer();
+    fetchHits();
     
 }
 
-function galleryItemIcon(galleryItems) {
-    console.log(galleryItems)
-    return galleryItems.map(
-        ({ webformatURL, tags, likes, views, comments, downloads }) =>
-          ` <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-          <div class="info">
-            <p class="info-item">
-              <b>${likes}</b>
-            </p>
-            <p class="info-item">
-              <b>${views}</b>
-            </p>
-            <p class="info-item">
-              <b>${comments}</b>
-            </p>
-            <p class="info-item">
-              <b>${downloads}</b>
-            </p>
-          </div>
-        </div>`
-      );
+function fetchHits() {
+    loadMoreBtn.disable();
+    newsApiService.fetchArticles().then(hits => {
+        appendHitsMarkup(hits);
+        loadMoreBtn.enable();
+    });
 }
 
-function insertContent(gallery) {
-    if (gallery.length === 1) {
-        resetMarkup(input);
-        form.innerHTML = galleryItemIcon(gallery);
-    } else {
-        resetMarkup(form);
-        input.innerHTML = createGalleryItemsMarkup(gallery);
-    }
-  };
+function appendHitsMarkup(hits) {
+    renderGallary(hits);
+}
 
-const getPosts = async () => {
-    onSubmitForm()
-    try {
-        console.log(response.data)
-        const response = await axios.get(URL);
-        if (data.length > 10) {
-            Notiflix.Notify.info(
-              'Too many matches found. Please enter a more specific name'
-            );
-            return;
-            }
-            
-        insertContent(response.data);
+function clearGalleryContainer() {
+    gallery.innerHTML = '';
+}
+
+
+
+
+
+
+
        
 
-    } catch {
-        resetMarkup(text);
-        resetMarkup(form);
-        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    }
-}
-
-form.addEventListener('submit', getPosts);
-
-const onSubmitForm = (e) => {
-    e.preventDefault();
-    inputText = e.currentTarget.input.value.trim();
-    if(!inputText) {
-        resetMarkup(text);
-        resetMarkup(form);
-        return;
-    }
-}
-
-function resetMarkup(el) {
-    el.innerHTML = '';
-  }
 
 
 
---
+++ src/js/gallery.js
@@ -0,0 +1,33 @@
+//import SimpleLightbox from 'simplelightbox';
+export {renderGallary}
+const gallary = document.querySelector('.gallery')
+
+function renderGallary (photo) {
+    const gallaryRander = photo
+    .map(photo => {const 
+      {webformatURL, largeImageURL, tags, likes, views, comments, downloads} = photo;
+ return `<div class="photo-card">
+ <a class="gallery__item" href="${largeImageURL}">
+        <img class="photoImg" src="${webformatURL}" alt="${tags}" loading="lazy"/>
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </div>
+      `;
+    }
+    )
+    gallary.insertAdjacentHTML('beforeend', gallaryRander.join(''));
+    // simpleLightBox.refresh();
+};
